c16ecc7c9e69fe11d823e5dd9ebd656755c18583
Mohamed <ahnbi32@gmail.com>
2025-01-19 11:15:15 +0100

    First commit

---
+++ chrome/2_reading-time/content.js
@@ -0,0 +1,23 @@
+const article = document.querySelector('article');
+
+if(article){
+    const text = article.textContent;
+    const wordMatchRegExp = /[^\s]+/g;
+    const words = text.matchAll(wordMatchRegExp);
+      // matchAll returns an iterator, convert to array to get word count
+    const wordCount = [...words].length;
+    const readingTime = Math.round(wordCount / 200);
+    const badge = document.createElement("p");
+    badge.classList.add("color-secondary-text", "type--caption");
+    badge.textContent = `⏱️ ${readingTime} min read`
+
+    const heading = article.querySelector("h1");
+    const date = article.querySelector("time")?.parentNode;
+
+    /*Si date est défini (non null ou undefined), alors (date ?? heading) renverra date.
+    Si date est null ou undefined, alors (date ?? heading) renverra heading.*/
+    
+    (date ?? heading).insertAdjacentElement("afterend", badge);
+
+
+}
